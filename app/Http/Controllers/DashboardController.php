223aa6c759eb9bd5358b16a075a659539203ccdd
RatanakK2R <kkeoratanak19@gmail.com>
2024-08-23 20:08:32 +0700

    Dashbord view successful

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Comment;
+use App\Models\Movie;
+use App\Models\Review;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 
@@ -15,10 +19,19 @@ class DashboardController extends Controller
     //  {
     //     $this->middleware(['auth']);
     //  }
-    public function __invoke(Request $request)
+    // public function __invoke(Request $request)
+    // {
+    //     return view('dashboard');
+    // }
+    public function index()
     {
-        return view('dashboard');
+        $totalMovies = Movie::count();
+        $totalReviews = Review::count();
+        $totalComment = Comment::count();
+        $totalUsers = User::count();
+        $recentComments = Comment::latest()->limit(5)->get(); // Fetch recent comments
+
+        return view('dashboard.index', compact('totalMovies', 'totalReviews', 'totalUsers', 'totalComment', 'recentComments'));
     }
 
-    
 }
--- routes/web.php
+++ routes/web.php
@@ -25,11 +25,12 @@ use Illuminate\Support\Facades\Route;
 //     // Route::get('/admin/dashboard', [AdminController::class, 'dashboard']);
 // });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'admin',])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'admin',])->name('dashboard');
 
-// Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+//Movies Dashboard
+Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard')->middleware(['auth', 'admin',]);
 
 Route::get('/', function () {
     return view('index');
